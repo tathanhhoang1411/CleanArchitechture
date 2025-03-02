b97aacca55af5c94d32d6a8f77e23cb25da3df57
HOÀNG TẠ <tathanhhoang.work@gmail.com>
2025-03-03 00:08:22 +0700

    orderby

--- CleanArchitechture-master/CleanArchitechture-master/CleanArchitecture.Infrastructure/Repositories/ProductRepository.cs
+++ CleanArchitechture-master/CleanArchitechture-master/CleanArchitecture.Infrastructure/Repositories/ProductRepository.cs
@@ -32,6 +32,7 @@ namespace CleanArchitecture.Infrastructure.Repositories
                 .Where(p => p.ProductName.Contains(data))
                 .Take(take)
                 .Skip(skip)
+                .OrderBy(p => p.CreatedAt)
                 .AsNoTracking()
                 .ToListAsync();
             return _mapper.Map<List<ProductDto>>(products);
